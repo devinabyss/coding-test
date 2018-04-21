1e7315331d07778e0d8ae78e56ba3887589d03f7
Sweet Melon <dwuthk@gmail.com>
2018-04-21 21:25:14 +0900

    - Programmers - level 3 - JumpCase - Programmers - level 3 - NextBigNumber - Programmers - level 3 - NLCM - Programmers - level 3 - Caesar

--- src/main/java/programmers/level3/JumpCase.java
+++ src/main/java/programmers/level3/JumpCase.java
@@ -1,30 +1,29 @@
 package programmers.level3;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
 /**
  * Created by HK on 2018-04-12.
  */
 public class JumpCase {
+    private static final Logger logger = LoggerFactory.getLogger(JumpCase.class);
+
     public int jumpCase(int num) {
-        int answer = 0;
 
-        boolean isEven = num % 0 == 0;
+        return num < 2 ? 1 : jumpCase(num-1) + jumpCase(num-2);
 
-        int maxTwice = num / 2;
-
-        int case1Twice = maxTwice + 2;
-
-
-
-
-
-
-        return answer;
     }
 
     public static void main(String[] args) {
         JumpCase c = new JumpCase();
-        int testCase = 4;
+        int[] testCase = {4, 5, 6, 21};
         //아래는 테스트로 출력해 보기 위한 코드입니다.
-        System.out.println(c.jumpCase(testCase));
+        Arrays.stream(testCase).forEach(tc -> {
+            System.out.println(c.jumpCase(tc));
+        });
+
     }
 }
