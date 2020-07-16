38d73a2edbe91d4d2c0c2894e2e18f991e627692
devh <dwuthk@gmail.com>
2020-07-17 00:53:34 +0900

    - HashMapTest 결과 좀 더 구체화. put method 분석해야 함.

--- src/main/java/datastructure/implemantation/HashMapTest.java
+++ src/main/java/datastructure/implemantation/HashMapTest.java
@@ -6,7 +6,9 @@ import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 public class HashMapTest {
@@ -14,28 +16,49 @@ public class HashMapTest {
 
     @Test
     public void test() {
-        Data a = new Data("A", "B");
-        Data b = new Data("A", "B");
 
-        Set<Data> set = new HashSet<>();
-        set.add(a);
-        set.add(b);
+        EqualsTrueHashNotEqual testCase1A = new EqualsTrueHashNotEqual("A", "B");
+        EqualsTrueHashNotEqual testCase1B = new EqualsTrueHashNotEqual("B", "B");
+        Map<EqualsTrueHashNotEqual, String> map1 = new HashMap<>();
+        map1.put(testCase1A, "AAA");
+        map1.put(testCase1B, "BBB");
 
-        log.info("## Set : {}", set);
+        log.info("##");
+        log.info("## Hash != , Equals == ##");
+        log.info("## TestCase1A : {}, TestCase1B : {}", testCase1A, testCase1B);
+        log.info("## Map : Size : {}, {}", map1.size(), map1);
+
+        EqualsTrueHashEqual testCase2A = new EqualsTrueHashEqual("A", "B");
+        EqualsTrueHashEqual testCase2B = new EqualsTrueHashEqual("A", "B");
+        Map<EqualsTrueHashEqual, String> map2 = new HashMap<>();
+        map2.put(testCase2A, "AAA");
+        map2.put(testCase2B, "BBB");
+
+        log.info("##");
+        log.info("## Hash == , Equals == ##");
+        log.info("## TestCase2A : {}, TestCase2B : {}", testCase2A, testCase2B);
+        log.info("## Map : Size : {}, {}", map2.size(), map2);
+
+        EqualsFalseHashEqual testCase3A = new EqualsFalseHashEqual("A", "B");
+        EqualsFalseHashEqual testCase3B = new EqualsFalseHashEqual("A", "B");
+
+        Map<EqualsFalseHashEqual, String> map3 = new HashMap<>();
+        map3.put(testCase3A, "AAA");
+        map3.put(testCase3B, "BBB");
+
+        log.info("##");
+        log.info("## Hash == , Equals != ##");
+        log.info("## TestCase3A : {}, TestCase3B : {}", testCase3A, testCase3B);
+        log.info("## Map : Size : {}, {}", map3.size(), map3);
 
 
-        Map<Data, String> map = new HashMap<>();
-        map.put(a, "AAA");
-        map.put(b, "BBB");
-
-        log.info("## Map : {}", map);
     }
 
 
     @AllArgsConstructor
     @Getter
     @ToString
-    public static class Data {
+    public static class EqualsTrueHashNotEqual {
         private final String propA;
         private final String propB;
 
@@ -46,7 +69,58 @@ public class HashMapTest {
 
         @Override
         public boolean equals(Object a) {
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName() + "::propA=" + propA + "::propB=" + propB + "::hashCode=" + hashCode();
+        }
+    }
+
+    @AllArgsConstructor
+    @Getter
+    @ToString
+    public static class EqualsTrueHashEqual {
+        private final String propA;
+        private final String propB;
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(propA, propB);
+        }
+
+        @Override
+        public boolean equals(Object a) {
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName() + "::propA=" + propA + "::propB=" + propB + "::hashCode=" + hashCode();
+        }
+    }
+
+    @AllArgsConstructor
+    @Getter
+    @ToString
+    public static class EqualsFalseHashEqual {
+        private final String propA;
+        private final String propB;
+
+        @Override
+        public int hashCode() {
+            return 11;
+        }
+
+        @Override
+        public boolean equals(Object a) {
             return false;
+        }
+
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName() + "::propA=" + propA + "::propB=" + propB + "::hashCode=" + hashCode();
         }
     }
 }
